ed5a679d6370e1acf9e64577cdb2d6b68a6afd79
Olya <olyamoruga@yandex.ru>
2024-05-27 12:23:43 +0300

    added the 2nd and the 4th tasks

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Enter a first number: ");
+int numFirst = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Enter a second number: ");
+int numSecond = int.Parse(Console.ReadLine()??"0");
+
+if (numFirst > numSecond)
+{
+    Console.WriteLine("The first number is greated than the second");
+}
+
+else
+{
+    Console.WriteLine("The second number is greater than the first");
+}
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Enter a first number: ");
+int numFirst = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Enter a second number: ");
+int numSecond = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Enter a third number: ");
+int numThird = int.Parse(Console.ReadLine()??"0");
+
+if (numFirst > numSecond && numFirst > numThird)
+{
+    Console.WriteLine("The first number is the greatest");
+}
+
+else if (numSecond > numFirst && numSecond > numThird)
+{
+    Console.WriteLine("The second number is the greatest");
+}
+else if (numThird > numFirst && numThird > numSecond)
+{
+    Console.WriteLine("The third number is the greatest");
+}
+
+else 
+{
+    Console.WriteLine("The numbers are even. Enter another numbers.");
+}
