2f86cdc594f8ca6657af1d5f5e26862db494ab77
Olya <olyamoruga@yandex.ru>
2024-05-27 12:30:29 +0300

    added the 6th task

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter a number: ");
+
+int num = int.Parse(Console.ReadLine()??"0");
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("The number " + num + " is even");
+}
+
+else 
+{
+    Console.WriteLine("The number " + num + " is odd");
+}
